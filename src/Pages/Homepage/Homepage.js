f0ad1535ab13d740205039ea244acb4706254f5f
syed-shaida-hussain <85978159+syed-shaida-hussain@users.noreply.github.com>
2022-10-29 12:25:31 +0530

    Added homepage

---
+++ src/Pages/Homepage/Homepage.js
@@ -0,0 +1,14 @@
+import "./Homepage.css"
+import "../../Utils/utils.css"
+const Homepage = () => {
+    return (<div>
+        <h1 className="highlighted mb1 mt1 ml1">DoUknow ?</h1>
+        <hr className="bg-primary mb1" />
+        <h1 className="centered mb1"><span className="text">Welcome to</span> <span className="highlighted">DoUknow</span></h1>
+        <p className="text centered fs-medium mb1"><span className="highlighted">DoUknow</span> is an interactive web app which provides facility to play online Quizes related to <span className="highlighted">Marvel and Gaming</span> . </p>
+        <p className="text centered fs-medium mb1">Take this quiz if you want to test your knowledge and learn some new stuff.</p>
+        <div className="centered"><button className=" btn cta-button"> Let's play</button></div>
+    </div>)
+}
+
+export {Homepage}
